7ed9f96438c023daafd4a8e22ccddd93217660cc
Rajesh Kumar Ranjan <rajesh.ranjan@lntedutech.com>
2024-07-02 16:04:08 +0530

    Update video_assessment.js

--- public/sdk/video_assessment.js
+++ public/sdk/video_assessment.js
@@ -2412,7 +2412,7 @@
                                 return t;
                             },
                             sync: (e) => (void 0 !== e ? new Promise((t) => t((Be = e))) : f("/api/room/fetch").then((e) => e && (Be = e))),
-                            start: () => (Ke.get("id") ? f("/api/room/start?id=".concat(Ke.get("id")), { method: "POST" ,body :{ipaddress: window.localStorage.getItem("ipaddress")}}).then((e) => (Be = e)) : new Promise((e, t) => t(new Error("Room ID not defined")))),
+                            start: () => (Ke.get("id") ? f("/api/room/start?id=".concat(Ke.get("id"))+'&status='.concat(Ke.get("status")), { method: "POST" ,body :{ipaddress: window.localStorage.getItem("ipaddress")}}).then((e) => (Be = e)) : new Promise((e, t) => t(new Error("Room ID not defined")))),
                             stop: () => (Ke.get("id") ? f("/api/room/stop?id=".concat(Ke.get("id")), { method: "POST" }).then((e) => (Be = e)) : new Promise((e, t) => t(new Error("Room ID not defined")))),
                             next() {
                                 let e = arguments.length > 0 && void 0 !== arguments[0] ? arguments[0] : null;
