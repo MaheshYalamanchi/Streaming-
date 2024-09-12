ff1bc7cd54feadeeb0a9fa146aeefd0ffc4c237c
Rajesh Kumar Ranjan <rajesh.ranjan@lntedutech.com>
2024-09-12 08:18:47 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 var bodyparser = require("body-parser");
 var cluster = require("cluster");
 var compression = require("compression");
